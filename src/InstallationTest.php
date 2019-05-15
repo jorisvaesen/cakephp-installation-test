8d9ee5ce41bd0242c14c910ae9af354ed3d857cf
Joris Vaesen <joris@datrix.be>
2019-05-15 12:43:06 +0200

    io write option

--- src/InstallationTest.php
+++ src/InstallationTest.php
@@ -2,13 +2,20 @@
 namespace InstallationTest;
 
 
+use Composer\IO\IOInterface;
 use InstallationTest\Exception\TestFailedException;
 use InstallationTest\Tester\TesterInterface;
 
 class InstallationTest
 {
+    private $io;
     private $testers = [];
     private $errors = [];
+
+    public function __construct(IOInterface $io = null)
+    {
+        $this->io = $io;
+    }
 
     public function registerTester(TesterInterface $tester)
     {
@@ -18,13 +25,17 @@ class InstallationTest
     /**
      * @inheritDoc
      */
-    public function test()
+    public function test($print = true)
     {
         try {
             foreach ($this->testers as $tester) {
                 $tester->test();
             }
         } catch (TestFailedException $ex) {
+            if ($print) {
+                $this->io->write('<error>' . $ex->getMessage() . '</error>');
+            }
+
             $this->errors[] = $ex->getMessage();
         }
 
